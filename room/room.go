9d55d1b15530cde493598805355ebad38548d70b
XihongKe <1102466499@qq.com>
2020-02-08 23:17:05 +0800

    Update room.go

--- room/room.go
+++ room/room.go
@@ -4,8 +4,9 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
+	"golang.org/x/net/websocket"
 	"math/rand"
-	"net/http"
 	"server/database"
 	"server/helper"
 	"server/user"
@@ -24,51 +25,93 @@ type Info struct {
 	User map[string]user.Info
 }
 
-func Create(w http.ResponseWriter, r *http.Request){
-	m := helper.BodyToStringMap(r)
+func Chat(ws *websocket.Conn){
+	var err error
+	var info Info
+	for {
+		var body string
+		var result []byte
+
+		if err = websocket.Message.Receive(ws, &body); err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		m := make(map[string]string)
+		if err = json.Unmarshal([]byte(body), &m); err != nil {
+			fmt.Println("json解析错误：" + err.Error())
+			continue
+		}
+
+		switch m["method"] {
+		case "create":
+			result = create(m, &info)
+			break
+		case "join":
+			result = join(m, &info)
+			break
+		case "send":
+			result = send(ws, m)
+			break
+		default:
+			result = []byte("{'code': 500;'msg' : 'wrong msg format'}")
+		}
+
+		if err = websocket.Message.Send(ws, string(result)); err != nil {
+			fmt.Println("下发数据时错误：" + err.Error())
+			continue
+		}
+	}
+}
+
+func send(ws *websocket.Conn, m map[string]string){
+
+}
+
+func create(m map[string]string, info *Info) (result []byte){
 	if _ ,ok := m["user_id"]; !ok {
-		helper.Respond(helper.ErrInvalidRequest, nil);return
+		return helper.Respond(helper.ErrInvalidRequest, nil)
 	}
 	userInfoByte := database.Get(m["user_id"])
 	userInfo := user.Info{}
 	if userInfoByte == nil {
-		helper.Respond(helper.ErrUserNotFound, nil);return
+		return helper.Respond(helper.ErrUserNotFound, nil)
 	}
 	json.Unmarshal(userInfoByte, &userInfo)
-	room := Info{createId(), statusReady, make(map[string]user.Info)}
-	room.User[userInfo.Id] = userInfo
-	database.Set(room.Id, room)
-	helper.HttpRespond(w, helper.Success, room)
+	info = &Info{createId(), statusReady, make(map[string]user.Info)}
+	info.User[userInfo.Id] = userInfo
+	database.Set(info.Id, info)
+	return helper.Respond(helper.Success, info)
 }
 
-func Join(w http.ResponseWriter, r *http.Request){
-	m := helper.BodyToStringMap(r)
+func join(m map[string]string, info *Info) (result []byte){
 	if _ ,ok := m["user_id"]; !ok {
-		helper.Respond(helper.ErrInvalidRequest, nil);return
+		return helper.Respond(helper.ErrInvalidRequest, nil)
 	}
 	if _ ,ok := m["room_id"]; !ok {
-		helper.Respond(helper.ErrInvalidRequest, nil);return
+		return helper.Respond(helper.ErrInvalidRequest, nil)
 	}
 
 	roomByte := database.Get(m["room_id"])
 	room := Info{}
 	if roomByte == nil {
-		helper.Respond(helper.ErrRoomNotFound, nil);return
+		return helper.Respond(helper.ErrRoomNotFound, nil)
 	}
 	json.Unmarshal(roomByte, &room)
 	if room.Status != statusReady{
-		helper.Respond(helper.ErrRoomUnavailable, nil);return
+		return helper.Respond(helper.ErrRoomUnavailable, nil)
 	}
 
 	userInfoByte := database.Get(m["user_id"])
 	userInfo := user.Info{}
 	if userInfoByte == nil {
-		helper.Respond(helper.ErrUserNotFound, nil);return
+		return helper.Respond(helper.ErrUserNotFound, nil)
 	}
 
 	room.User[userInfo.Id] = userInfo
 	database.Set(room.Id, room)
-	helper.Respond(helper.Success, room)
+	info = &room
+	return helper.Respond(helper.Success, room)
 }
 
 func createId() string{
